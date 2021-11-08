f176f7b9d1cb38009159fcaf736b79b9669419c3
unknown <n0ruje00@students.oamk.fi>
2021-11-08 11:32:13 +0200

    Tallenus tehty

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+header('Acces-Control-Allow-Origin:*');
+header('Content-Type: application/json');
+
+$db = New PDO('mysql:host=localhost;dbname=todo;charset=utf8','root','');
+$sql = "select * from task";
+$query = $db->query($sql);
+$results = $query->fetchAll(PDO::FETCH_ASSOC);
+header('HTTP/1.1. 200 OK');
+print json_encode($results);
